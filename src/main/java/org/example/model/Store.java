c311ee043d0eac0510f34bafd096b4a8b99accac
hmgny <hmerveguney@hotmail.com>
2025-01-17 14:32:39 +0300

    S14D4 tamamlandı

--- src/main/java/org/example/model/Store.java
+++ src/main/java/org/example/model/Store.java
@@ -2,11 +2,17 @@ package org.example.model;
 
 public class Store {
     public static void main(String[] args) {
+        ProductForSale[] products = new ProductForSale[3];
 
-        //listProducts();
+        products[0] = new Chocolate("Chocolate", 5.99, "Delicious dark chocolate", "Dark");
+        products[1] = new Coke("Coke", 1.99, "Refreshing soda", true);
+        products[2] = new Bread("Bread", 2.49, "Fresh whole wheat bread", "Whole Wheat");
+
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+        for(ProductForSale product: products){
+        }
     }
 }
